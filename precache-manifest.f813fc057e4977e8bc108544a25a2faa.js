c6e5218a62b2883798dbb32ba462ee2b4e7814c5
Sriram <46366477+ram170@users.noreply.github.com>
2020-05-02 16:24:07 +0530

    Updates

---
+++ precache-manifest.f813fc057e4977e8bc108544a25a2faa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c936ebb5fc1168aee914d7faff7feab",
+    "url": "/ToDoReact/index.html"
+  },
+  {
+    "revision": "66211d5e081b2f70abd1",
+    "url": "/ToDoReact/static/css/main.b3a7963d.chunk.css"
+  },
+  {
+    "revision": "5e0e2078ad2657ffa2b0",
+    "url": "/ToDoReact/static/js/2.3666ba70.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ToDoReact/static/js/2.3666ba70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66211d5e081b2f70abd1",
+    "url": "/ToDoReact/static/js/main.ebe316bb.chunk.js"
+  },
+  {
+    "revision": "7ba3fbd33f38f65cd7b9",
+    "url": "/ToDoReact/static/js/runtime-main.d1920278.js"
+  }
+]);
